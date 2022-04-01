948d64778edd3607c2c52fddaa6fe44cea3003aa
Thiago Cedran Santos <thiagocdn@gmail.com>
2022-03-31 22:29:50 -0300

    desafio finalizado

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,32 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle || newTaskTitle === '') return
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false,
+    }
+
+    setTasks([...tasks, newTask]);
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const newTasksList = tasks.map(task => {
+      if(task.id === id) return {
+        ...task,
+        isComplete: !task.isComplete
+      }
+      return task
+    })
+    setTasks(newTasksList)
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newTasksList = tasks.filter(task => task.id !== id)
+    setTasks(newTasksList)
   }
 
   return (
